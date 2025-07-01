4501e8563c45e6a34c9c9ca0065ec3ccb798a1f6
alihadi202 <your_email@example.com>
2025-07-02 00:59:14 +0300

    learnt a lot tailwind is acting up tho

---
+++ next-app/app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <div><button onClick={() =>console.log('Click')}>tung tung tung</button></div>
+  )
+}
+
+export default AddToCard
